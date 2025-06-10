819824747c27945eb3022ba78ddd14214a25e6ee
SHANKAR-YADAVA <shankaryadavar2004@gmail.com>
2025-06-10 18:13:20 +0530

    landing page and clerk authentication done

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,8 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
-
+import { Toaster } from "sonner";
+import Header from "@/components/header";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,12 +13,19 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
+    <ClerkProvider>
     <html lang="en">
-      <body
-        className={`$(inter.className)`}
-      >
-        {children}
+      <body className={`${inter.className}`}>
+        <Header/>
+        <main className="min-h-screen">{children}</main>
+        <Toaster richColors />
+        <footer className="bg-blue-50 py-12">
+          <div className="container mx-auto px-4 text-center text-gray-600">
+            <p>Made with 💗 by Welth Team</p>
+          </div>
+        </footer>
       </body>
     </html>
+    </ClerkProvider>
   );
 }
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
+// next.config.js
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ['randomuser.me'],
+  },
+};
 
 export default nextConfig;
