be448b3d2ff24fad698b519910e916ceeb957ff1
SHANKAR-YADAVA <shankaryadavar2004@gmail.com>
2025-06-12 16:42:25 +0530

    arcject applied for rate limiting of 10 requests of transaction per day

--- actions/transaction.js
+++ actions/transaction.js
@@ -3,6 +3,8 @@
 import { auth } from "@clerk/nextjs/server";
 import { db } from "@/lib/prisma";
 import { revalidatePath } from "next/cache";
+import aj from "@/lib/arcject";
+import { request } from "@arcjet/next";
 
 
 const serializeAmount = (obj) => ({
@@ -15,6 +17,33 @@ export async function createTransaction(data) {
   try {
     const { userId } = await auth();
     if (!userId) throw new Error("Unauthorized");
+
+      // Get request data for ArcJet
+    const req = await request();
+
+    // Check rate limit
+    const decision = await aj.protect(req, {
+      userId,
+      requested: 1, // Specify how many tokens to consume
+    });
+
+    if (decision.isDenied()) {
+      if (decision.reason.isRateLimit()) {
+        const { remaining, reset } = decision.reason;
+        console.error({
+          code: "RATE_LIMIT_EXCEEDED",
+          details: {
+            remaining,
+            resetInSeconds: reset,
+          },
+        });
+
+        throw new Error("Too many requests. Please try again later.");
+      }
+
+      throw new Error("Request blocked");
+    }
+
 
 
     const user = await db.user.findUnique({
